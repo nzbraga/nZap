fd8f56932b15255469051eb90f5bca7ade29813b
Neylson Braga Gomes <58315795+nzbraga@users.noreply.github.com>
2025-05-16 13:25:56 -0300

    0.2

---
+++ app.py
@@ -0,0 +1,15 @@
+from assets.interface.telas.tela_login.tela_login import tela_login
+#from assets.func.sessao.reiniciar_sessao import limpar_data
+
+def main():
+    #limpar_data()
+
+
+    #Inicializa o programa, chamando a fun o 'mostrar_tela' que 
+    #renderiza a tela principal do aplicativo.
+    
+    tela_login()
+
+if __name__ == "__main__":
+    main()
+
